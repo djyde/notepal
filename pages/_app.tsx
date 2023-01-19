a21ac03f418674311afc139eed6b6946f2d5c88f
Randy <randypriv@gmail.com>
2023-01-20 01:41:13 +0800

    disable zoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>Page title</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no" />
       </Head>
 
       <MantineProvider
