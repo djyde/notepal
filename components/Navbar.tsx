5b0dd3df336c97382c5ce7b597804838902fb211
Randy <randypriv@gmail.com>
2023-01-20 16:19:22 +0800

    add group

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,9 @@
-import { Box, Group, Space, Text } from "@mantine/core";
+import { Box, Group, Paper, Space, Stack, Text, Title } from "@mantine/core";
+import { openModal } from "@mantine/modals";
+import Image from "next/image";
 import { ReactNode } from "react";
 import { mq } from "../utils";
+import wxgroupImage from '../public/wxgroup.jpg'
 
 export function Navbar(props: {
   logo: ReactNode
@@ -44,6 +47,27 @@ export function Navbar(props: {
             }
           })}>
             源码
+          </Text>
+          <Text sx={{
+            cursor: 'pointer'
+          }} onClick={_ => {
+            openModal({
+              // title: '反馈',
+              centered: true,
+              children: (
+                <Box>
+                  <Stack align={'start'}>
+                    <Text>如果你有需求提议，欢迎加入群组反馈。</Text>
+                    <Title order={4}>Telegram 群组</Title>
+                    <Text component="a" href="https://t.me/+a7YMH3l8AFYzMDll">https://t.me/+a7YMH3l8AFYzMDll</Text>
+                    <Title order={4}>微信群组</Title>
+                    <Image width={256} height={256} alt="微信群二维码" src={wxgroupImage} />
+                  </Stack>
+                </Box>
+              )
+            })
+          }}>
+            反馈
           </Text>
         </Group>
       </Group>
