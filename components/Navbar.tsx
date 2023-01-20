38a9ddf7c9119b9008b01ab75b9961fb17578752
Randy <randypriv@gmail.com>
2023-01-20 15:05:53 +0800

    add nav link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Group, Text } from "@mantine/core";
+import { Box, Group, Space, Text } from "@mantine/core";
 import { ReactNode } from "react";
 import { mq } from "../utils";
 
@@ -10,22 +10,42 @@ export function Navbar(props: {
       padding: theme.spacing.md,
       borderBottom: `1px solid ${theme.colors.gray[8]}`,
     })}>
-      <Group spacing={'xs'} sx={theme => ({
+      <Group sx={theme => ({
         [mq(theme, 'md')]: {
           padding: 0,
           width: 640,
           margin: '0 auto',
         }
       })}>
-        <Text variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 45 }} sx={theme => ({
-          color: theme.colors.blue[7],
-          // fontSize: '1.5rem',
-          fontWeight: 'bold'
-        })}>
-          {props.logo}
-        </Text>
-        <Text size='sm'>by <Text underline component="a" href="https://lutaonan.com">RandySoft</Text>
-        </Text>
+        <Group spacing={'xs'}>
+          <Text variant="gradient" gradient={{ from: 'indigo', to: 'cyan', deg: 45 }} sx={theme => ({
+            color: theme.colors.blue[7],
+            // fontSize: '1.5rem',
+            fontWeight: 'bold'
+          })}>
+            {props.logo}
+          </Text>
+          <Text size='sm'>by <Text underline component="a" href="https://lutaonan.com">RandySoft</Text>
+          </Text>
+        </Group>
+        <Group sx={{
+          marginLeft: 'auto'
+        }}>
+          <Text component="a" target={"_blank"} href="https://www.bilibili.com/video/BV1q3411d7Gj/" sx={theme => ({
+            transition: 'color 0.2s ease',
+            ":hover": {
+              color: theme.colors.blue[7],
+            }
+          })}>使用教程</Text>
+          <Text component="a" target={'_blank'} href="https://github.com/djyde/notepal" sx={theme => ({
+            transition: 'color 0.2s ease',
+            ":hover": {
+              color: theme.colors.blue[7],
+            }
+          })}>
+            源码
+          </Text>
+        </Group>
       </Group>
     </Box>
   )
